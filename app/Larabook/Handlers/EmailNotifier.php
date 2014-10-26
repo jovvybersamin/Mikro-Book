899d40bceeaeedaa11b4e514fc2ba921e0494801
Jovvy Bersamin <jovvybersamin99@gmail.com>
2014-10-26 13:54:34 +0000

    .

---
+++ app/Larabook/Handlers/EmailNotifier.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mikro
+ * Date: 10/26/2014
+ * Time: 9:51 PM
+ */
+
+namespace Larabook\Handlers;
+
+
+class EmailNotifier {
+
+} 
---
+++ app/Larabook/Users/FollowableTrait.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mikro
+ * Date: 10/21/2014
+ * Time: 5:40 AM
+ */
+
+namespace Larabook\Users;
+
+
+trait FollowableTrait {
+
+} 
