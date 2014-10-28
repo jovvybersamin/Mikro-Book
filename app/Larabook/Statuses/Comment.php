f121179b27a18071ff59dcfbcd7ccff3d04aa672
Jovvy Bersamin <jovvybersamin99@gmail.com>
2014-10-28 14:02:52 +0000

    added comment system for statuses

---
+++ app/Larabook/Statuses/Comment.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mikro
+ * Date: 10/28/2014
+ * Time: 8:07 PM
+ */
+
+namespace Larabook\Statuses;
+
+
+class Comment {
+
+} 
---
+++ app/Larabook/Statuses/Presenter/CommentPresenter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mikro
+ * Date: 10/28/2014
+ * Time: 9:29 PM
+ */
+
+namespace Larabook\Statuses\Presenter;
+
+
+class CommentPresenter {
+
+} 
